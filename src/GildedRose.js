57d5a47ce74857c6df47c172e0fb695295af9714
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-10-06 09:39:24 +0200

    constant name list for naming consistency

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,40 +1,49 @@
 import Item from './Item'
 
+const itemNames = {
+  VEST: '+5 Dexterity Vest',
+  BRIE: 'Aged Brie',
+  MONGOOSE: 'Elixir of the Mongoose',
+  SULFURAS: 'Sulfuras, Hand of Ragnaros',
+  TAFKAL80ETC: 'Backstage passes to a TAFKAL80ETC concert',
+  CONJURED: 'Conjured Mana Cake'
+}
+
 const GildedRose = function () {
   var items = []
-  items.push(new Item("+5 Dexterity Vest", 10, 20))
-  items.push(new Item("Aged Brie", 2, 0))
-  items.push(new Item("Elixir of the Mongoose", 5, 7))
-  items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80))
-  items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20))
-  items.push(new Item("Conjured Mana Cake", 3, 6))
+  items.push(new Item(itemNames.VEST, 10, 20))
+  items.push(new Item(itemNames.BRIE, 2, 0))
+  items.push(new Item(itemNames.MONGOOSE, 5, 7))
+  items.push(new Item(itemNames.SULFURAS, 0, 80))
+  items.push(new Item(itemNames.TAFKAL80ETC, 15, 20))
+  items.push(new Item(itemNames.CONJURED, 3, 6))
   GildedRose.updateQuality(items)
 }
 
 GildedRose.updateQuality = function (items) {
   for (const item of items) {
-    if ("Aged Brie" !== item.name && "Backstage passes to a TAFKAL80ETC concert" !== item.name) {
+    if (itemNames.BRIE !== item.name && itemNames.TAFKAL80ETC !== item.name) {
       //TODO: Improve this code.
       if (item.quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" !== item.name) {
+        if (itemNames.SULFURAS !== item.name) {
           item.quality--
         }
       }
     } else {
       if (item.quality < 50) {
         item.quality++
-        if ("Aged Brie" === item.name) {
-            if (item.sellIn < 6) {
-              item.quality++
-            }
+        if (itemNames.BRIE === item.name) {
+          if (item.sellIn < 6) {
+            item.quality++
+          }
         }
         //Increases the Quality of the stinky cheese if its 11 days to due date.
-        if ("Aged Brie" === item.name) {
-            if (item.sellIn < 11) {
-              item.quality++
-            }
+        if (itemNames.BRIE === item.name) {
+          if (item.sellIn < 11) {
+            item.quality++
+          }
         }
-        if ("Backstage passes to a TAFKAL80ETC concert" === item.name) {
+        if (itemNames.TAFKAL80ETC === item.name) {
           if (item.sellIn < 11) {
             // See revision number 2394 on SVN.
             if (item.quality < 50) {
@@ -50,14 +59,14 @@ GildedRose.updateQuality = function (items) {
         }
       }
     }
-    if ("Sulfuras, Hand of Ragnaros" !== item.name) {
+    if (itemNames.SULFURAS !== item.name) {
       item.sellIn--
     }
     if (item.sellIn < 0) {
-      if ("Aged Brie" !== item.name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" !== item.name) {
+      if (itemNames.BRIE !== item.name) {
+        if (itemNames.TAFKAL80ETC !== item.name) {
           if (item.quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" !== item.name) {
+            if (itemNames.SULFURAS !== item.name) {
               item.quality--
             }
           }
@@ -69,11 +78,11 @@ GildedRose.updateQuality = function (items) {
         if (item.quality < 50) {
           item.quality++ // Not covered by tests
         }
-        if ("Aged Brie" === item.name && item.sellIn <= 0)
-            item.quality = 0
+        if (itemNames.BRIE === item.name && item.sellIn <= 0)
+          item.quality = 0
       } // of for.
     }
-    if ("Sulfuras, Hand of Ragnaros" !== item.name)
+    if (itemNames.SULFURAS !== item.name)
       if (item.quality > 50) item.quality = 50
   }
   return items
