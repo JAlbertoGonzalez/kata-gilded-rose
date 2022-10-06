f96207618f280aa6bc9bf56ed9131de4f7efc92a
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-10-06 14:20:23 +0200

    move items quality logic to item.js

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -11,86 +11,9 @@ const GildedRose = function () {
   GildedRose.updateQuality(items)
 }
 
-function AgedBrie(item) {
-  if (item.quality < 50) {
-    item.quality++;
-
-    if (item.sellIn < 6)
-      item.quality++
-
-  }
-
-  if (item.sellIn < 11)
-    item.quality++
-
-
-  item.sellIn--;
-
-  if (item.sellIn < 0)
-    item.quality = 0
-
-
-  if (item.quality > 50)
-    item.quality = 50
-
-  return item
-}
-
-function Tafkal(item) {
-  item.quality++
-  if (item.quality < 50) {
-    // See revision number 2394 on SVN.
-    if (item.sellIn < 11)
-      item.quality++
-
-    //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-    if (item.sellIn < 6)
-      item.quality++
-
-    item.sellIn--
-
-    if (item.sellIn < 0)
-      item.quality = 0
-  }
-
-  return item
-}
-
-function Sulfuras(item) {
-  // Inmutable item
-  return item
-}
-
-function Default(item) {
-  if (item.quality > 0)
-    item.quality--
-
-  item.sellIn--
-
-  if (item.sellIn < 0 && item.quality > 0)
-    item.quality--
-
-  if (item.quality > 50)
-    item.quality = 50
-
-  return item
-}
-
 GildedRose.updateQuality = function (items) {
   for (const item of items) {
-    switch (item.name) {
-      case ITEM_NAMES.BRIE:
-        AgedBrie(item)
-        break
-      case ITEM_NAMES.TAFKAL80ETC:
-        Tafkal(item)
-        break
-      case ITEM_NAMES.SULFURAS:
-        Sulfuras(item)
-        break
-      default:
-        Default(item)
-    }
+    item.updateQuality()
   }
   return items
 };
--- src/Item.js
+++ src/Item.js
@@ -7,10 +7,91 @@ export const ITEM_NAMES = {
   CONJURED: 'Conjured Mana Cake'
 }
 
+export function AgedBrie(item) {
+  if (item.quality < 50) {
+    item.quality++;
+
+    if (item.sellIn < 6)
+      item.quality++
+
+  }
+
+  if (item.sellIn < 11)
+    item.quality++
+
+
+  item.sellIn--;
+
+  if (item.sellIn < 0)
+    item.quality = 0
+
+
+  if (item.quality > 50)
+    item.quality = 50
+
+  return item
+}
+
+export function Tafkal(item) {
+  item.quality++
+  // See revision number 2394 on SVN.
+  if (item.sellIn < 11)
+    item.quality++
+
+  //Increases the Quality of Backstage Passes if the Quality is 6 or less.
+  if (item.sellIn < 6)
+    item.quality++
+
+  item.sellIn--
+
+  if (item.sellIn < 0)
+    item.quality = 0
+
+  if (item.quality > 50)
+    item.quality = 50
+
+  return item
+}
+
+export function Sulfuras(item) {
+  // Inmutable item
+  return item
+}
+
+export function Default(item) {
+  if (item.quality > 0)
+    item.quality--
+
+  item.sellIn--
+
+  if (item.sellIn < 0 && item.quality > 0)
+    item.quality--
+
+  if (item.quality > 50)
+    item.quality = 50
+
+  return item
+}
+
 export default class Item {
   constructor(name, sellIn, quality) {
     this.name = name
     this.sellIn = sellIn
     this.quality = quality
+  }
+  updateQuality() {
+    switch (this.name) {
+      case ITEM_NAMES.BRIE:
+        AgedBrie(this)
+        break
+      case ITEM_NAMES.TAFKAL80ETC:
+        Tafkal(this)
+        break
+      case ITEM_NAMES.SULFURAS:
+        Sulfuras(this)
+        break
+      default:
+        Default(this)
+    }
   }
 }
