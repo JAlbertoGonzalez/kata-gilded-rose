b8b6765be57effadf9b8ea4e5807308a716c4667
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-10-06 14:41:01 +0200

    comment quality logic

--- src/Item.js
+++ src/Item.js
@@ -11,21 +11,24 @@ export function AgedBrie(item) {
   if (item.quality < 50) {
     item.quality++;
 
+    // Quality increases by 3 when there are 5 days or less
     if (item.sellIn < 6)
       item.quality++
 
   }
 
+  // Quality increases by 2 when there are 10 days or less
   if (item.sellIn < 11)
     item.quality++
 
 
   item.sellIn--;
 
+  // Quality drops to 0 after the concert.
   if (item.sellIn < 0)
     item.quality = 0
 
-
+  // The Quality of an item is never more than 50
   if (item.quality > 50)
     item.quality = 50
 
@@ -34,19 +37,21 @@ export function AgedBrie(item) {
 
 export function Tafkal(item) {
   item.quality++
-  // See revision number 2394 on SVN.
+  // Quality increases by 2 when there are 10 days or less
   if (item.sellIn < 11)
     item.quality++
 
-  //Increases the Quality of Backstage Passes if the Quality is 6 or less.
+  // Quality increases by 3 when there are 5 days or less
   if (item.sellIn < 6)
     item.quality++
 
   item.sellIn--
 
+  // Quality drops to 0 after the concert.
   if (item.sellIn < 0)
     item.quality = 0
 
+  // The Quality of an item is never more than 50
   if (item.quality > 50)
     item.quality = 50
 
@@ -54,7 +59,12 @@ export function Tafkal(item) {
 }
 
 export function Sulfuras(item) {
-  // Inmutable item
+  // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality.
+
+  // The Quality of an item is never more than 50,
+  // but "Sulfuras" is a legendary item and as such
+  // its Quality is always 80 and it never alters.
+  item.quality = 80
   return item
 }
 
@@ -64,9 +74,11 @@ export function Default(item) {
 
   item.sellIn--
 
+  // Once the sell by date has passed, Quality degrades twice as fast.
   if (item.sellIn < 0 && item.quality > 0)
     item.quality--
 
+  // The Quality of an item is never more than 50
   if (item.quality > 50)
     item.quality = 50
 
@@ -79,6 +91,7 @@ export default class Item {
     this.sellIn = sellIn
     this.quality = quality
   }
+
   updateQuality() {
     switch (this.name) {
       case ITEM_NAMES.BRIE:
